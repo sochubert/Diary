116464c97b846f2d2c1a2f77402aa439721e5cad
sochubert <sochubert@gmail.com>
2022-01-07 12:54:43 +0900

    React Router

---
+++ src/pages/Edit.js
@@ -0,0 +1,20 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+const Edit = () => {
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const id = searchParams.get('id');
+    const mode = searchParams.get("mode");
+
+    return (
+        <div>
+            <h1>Edit</h1>
+            <p>이곳은 일기 수정 페이지 입니다.</p>
+            <button onClick={()=>setSearchParams({who: 'minjae'})}>QS 바꾸기</button>
+            <button onClick={()=>navigate("/home")}>홈으로 가기</button>
+            <button onClick={()=>navigate(-1)}>뒤로가기</button>
+        </div>
+    );
+};
+
+export default Edit;
